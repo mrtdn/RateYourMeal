64679659651e0a4fed75a200091047be3b126170
mrtdn <mrtdncr@outlook.com.tr>
2022-03-06 18:41:56 +0300

    updated

---
+++ lib/models/food_model.dart
@@ -0,0 +1,9 @@
+class Food {
+  final String food_name;
+  final String image_URL;
+
+  const Food({
+    required this.food_name,
+    required this.image_URL,
+  });
+}
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class RYM extends StatelessWidget {
@@ -12,7 +13,15 @@ class RYM extends StatelessWidget {
         scaffoldBackgroundColor: Colors.white,
         fontFamily: 'Comfortaa',
       ),
-      home: const HomePage(title: 'Temp title'),
+      home: const HomePage(
+          title: Text(
+        'Rate Your Meal',
+        style: TextStyle(
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+          fontSize: 29.0,
+        ),
+      )),
     );
   }
 }
@@ -20,7 +29,7 @@ class RYM extends StatelessWidget {
 class HomePage extends StatefulWidget {
   const HomePage({Key? key, required this.title}) : super(key: key);
 
-  final String title;
+  final Text title;
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -30,9 +39,57 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: widget.title,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: SizedBox(),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    child: Padding(
+                      child: Image.asset('assets/images/atatepe.jpg'),
+                      padding: const EdgeInsets.all(10.0),
+                    ),
+                    decoration: BoxDecoration(border: Border.all()),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Container(
+                    child: Padding(
+                      child: Image.asset('assets/images/parlar.jpg'),
+                      padding: const EdgeInsets.all(10.0),
+                    ),
+                    decoration: BoxDecoration(border: Border.all()),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text("Anasayfa"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text("Profil"),
+          ),
+        ],
       ),
     );
   }
